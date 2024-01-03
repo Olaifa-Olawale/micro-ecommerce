9846117ebfcb51e9f10714f45716674e1db32775
“Olaifa-Olawale” <olawale.favour10@gmail.com>
2024-01-03 11:04:47 -0600

    test

--- src/products/forms.py
+++ src/products/forms.py
@@ -3,7 +3,7 @@ from .models import Product
 
 
 #input_css_class = "form-control"
-input_css_class = "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+input_css_class = "bg-gray-700 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
 
 class ProductForm(forms.ModelForm):
     # name = forms.CharField(widget=forms.TextInput(attrs={"class":"form-control"}))
